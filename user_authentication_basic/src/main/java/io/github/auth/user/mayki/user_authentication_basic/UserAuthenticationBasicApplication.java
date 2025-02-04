a6f47ae8c0e633087b1fabcaeae097758c07141b
Maike2961 <mayki.silva@hotmail.com>
2025-02-03 21:46:41 -0300

    first commit

---
+++ user_authentication_basic/src/main/java/io/github/auth/user/mayki/user_authentication_basic/UserAuthenticationBasicApplication.java
@@ -0,0 +1,14 @@
+package io.github.auth.user.mayki.user_authentication_basic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+UserAuthenticationBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAuthenticationBasicApplication.class, args);
+	}
+
+}
---
+++ user_authentication_basic/src/test/java/io/github/auth/user/mayki/user_authentication_basic/UsuarioAuthenticationBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.auth.user.mayki.user_authentication_basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioAuthenticationBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
