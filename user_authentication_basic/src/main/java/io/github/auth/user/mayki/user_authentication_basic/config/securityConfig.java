d3543b017bb488f77ed7ebfa091329b889fe7f80
Maike2961 <mayki.silva@hotmail.com>
2025-06-03 15:35:31 -0300

    feat: add uma customização em grantedAuthorityDefaults

--- user_authentication_basic/src/main/java/io/github/auth/user/mayki/user_authentication_basic/config/securityConfig.java
+++ user_authentication_basic/src/main/java/io/github/auth/user/mayki/user_authentication_basic/config/securityConfig.java
@@ -12,46 +12,44 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-@EnableWebSecurity
 @Configuration
+@EnableWebSecurity
 public class securityConfig {
 
-    @Autowired
-    private securityFilter securityFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         return http
-                .csrf(csrf -> csrf.disable())
+                .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> {
-                    authorize.requestMatchers(HttpMethod.POST, "/users/**").permitAll();
+                    authorize.requestMatchers(HttpMethod.POST, "/users").permitAll();
                     authorize.requestMatchers(HttpMethod.GET, "/users/admin").hasRole("ADMIN");
                     authorize.requestMatchers(HttpMethod.GET, "/users/user").hasRole("USER");
-                    authorize.requestMatchers(HttpMethod.POST, "/auth/**").permitAll();
                     authorize.anyRequest().authenticated();
-                }).addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
+                })
                 .build();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
-
-
-    @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public GrantedAuthorityDefaults grantedAuthority(){
+        return new GrantedAuthorityDefaults("");
     }
 
 }
