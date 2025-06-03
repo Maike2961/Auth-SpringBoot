a7a5faace64099bc107634c78d57988bdcbb3739
Maike2961 <mayki.silva@hotmail.com>
2025-06-03 15:31:10 -0300

    feat: representando o usuário autenticado no Spring

--- user_authentication_basic/src/main/java/io/github/auth/user/mayki/user_authentication_basic/security/CustomAuthentication.java
+++ user_authentication_basic/src/main/java/io/github/auth/user/mayki/user_authentication_basic/security/CustomAuthentication.java
@@ -1,4 +1,59 @@
 package io.github.auth.user.mayki.user_authentication_basic.security;
 
-public class CustomAuthentication {
+import io.github.auth.user.mayki.user_authentication_basic.model.Usuario;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+public class CustomAuthentication implements Authentication {
+
+    private final Usuario usuario;
+
+    public CustomAuthentication(Usuario usuario) {
+        this.usuario = usuario;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.usuario
+                .getRole()
+                .stream()
+                .map(roles -> new SimpleGrantedAuthority(roles.toString()))
+                .collect(Collectors.toSet());
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getDetails() {
+        return usuario;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return usuario;
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return true;
+    }
+
+    @Override
+    public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {}
+
+    @Override
+    public String getName() {
+        return usuario.getLogin();
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
 }
