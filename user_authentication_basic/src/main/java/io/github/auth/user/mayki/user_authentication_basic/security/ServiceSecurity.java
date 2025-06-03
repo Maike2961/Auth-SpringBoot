f4bf98da6e4d670274413bf879c03f62637d67b5
Maike2961 <mayki.silva@hotmail.com>
2025-06-03 15:31:55 -0300

    feat: obtendo o usuario logado

--- user_authentication_basic/src/main/java/io/github/auth/user/mayki/user_authentication_basic/security/ServiceSecurity.java
+++ user_authentication_basic/src/main/java/io/github/auth/user/mayki/user_authentication_basic/security/ServiceSecurity.java
@@ -1,4 +1,19 @@
 package io.github.auth.user.mayki.user_authentication_basic.security;
 
+import io.github.auth.user.mayki.user_authentication_basic.model.Usuario;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component
 public class ServiceSecurity {
+
+    public Usuario getUsuarioLogado(){
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if(authentication instanceof CustomAuthentication customAuthentication){
+            return customAuthentication.getUsuario();
+        }
+        return null;
+    }
 }
