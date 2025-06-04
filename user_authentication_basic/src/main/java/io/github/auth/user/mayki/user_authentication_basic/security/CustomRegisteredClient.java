313078ea15ee39d9330f9af3d9815f8a908d601a
Maike2961 <mayki.silva@hotmail.com>
2025-06-04 13:34:31 -0300

    feat: add uma classe para encontrar o client

---
+++ user_authentication_basic/src/main/java/io/github/auth/user/mayki/user_authentication_basic/security/CustomRegisteredClient.java
@@ -0,0 +1,57 @@
+package io.github.auth.user.mayki.user_authentication_basic.security;
+
+import io.github.auth.user.mayki.user_authentication_basic.model.Client;
+import io.github.auth.user.mayki.user_authentication_basic.service.ClientService;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.ClientSettings;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomRegisteredClient implements RegisteredClientRepository {
+
+    private final ClientService clientService;
+    private final TokenSettings tokenSettings;
+    private final ClientSettings clientSettings;
+
+    public CustomRegisteredClient(ClientService clientService,
+                                  TokenSettings tokenSettings,
+                                  ClientSettings clientSettings) {
+        this.clientService = clientService;
+        this.tokenSettings = tokenSettings;
+        this.clientSettings = clientSettings;
+    }
+
+    @Override
+    public void save(RegisteredClient registeredClient) {}
+
+    @Override
+    public RegisteredClient findById(String id) {
+        return null;
+    }
+
+    @Override
+    public RegisteredClient findByClientId(String clientId) {
+
+        Client client = clientService.obterPorClientId(clientId);
+
+        if(client == null){
+            return null;
+        }
+        return RegisteredClient
+                .withId(client.getId().toString())
+                .clientId(client.getClientId())
+                .clientSecret(client.getClientSecret())
+                .redirectUri("http://localhost:8080/authorized")
+                .scope(client.getScope())
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .authorizationGrantType(AuthorizationGrantType.AUTHORIZATION_CODE)
+                .tokenSettings(tokenSettings)
+                .clientSettings(clientSettings)
+                .build();
+    }
+}
