072a31d19f6180bb25392dd0f75e13c677e3f89b
Maike2961 <mayki.silva@hotmail.com>
2025-06-06 19:44:47 -0300

    feat: add login permitAll

--- user_authentication_basic/src/main/java/io/github/auth/user/mayki/user_authentication_basic/config/securityConfig.java
+++ user_authentication_basic/src/main/java/io/github/auth/user/mayki/user_authentication_basic/config/securityConfig.java
@@ -1,25 +1,15 @@
 package io.github.auth.user.mayki.user_authentication_basic.config;
 
-import io.github.auth.user.mayki.user_authentication_basic.repository.UserRepository;
-import io.github.auth.user.mayki.user_authentication_basic.security.CustomUserDetailService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
@@ -35,15 +25,15 @@ public class securityConfig {
 
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(Customizer.withDefaults())
                 .formLogin(Customizer.withDefaults())
                 .authorizeHttpRequests(authorize -> {
+                    authorize.requestMatchers("/login").permitAll();
                     authorize.requestMatchers(HttpMethod.POST, "/users").permitAll();
                     authorize.anyRequest().authenticated();
                 })
-
-                .oauth2ResourceServer(oauth2Rs -> oauth2Rs.jwt(Customizer.withDefaults()))
+                .oauth2ResourceServer(oauth2Rs -> oauth2Rs
+                        .jwt(Customizer.withDefaults()))
                 .build();
     }
 
