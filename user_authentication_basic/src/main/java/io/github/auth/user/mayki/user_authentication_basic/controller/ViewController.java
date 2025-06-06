0a7d8119589af84f0a9b363b96c24c39fa5bfe97
Maike2961 <mayki.silva@hotmail.com>
2025-06-06 19:42:08 -0300

    feat: add controller authentication

--- user_authentication_basic/src/main/java/io/github/auth/user/mayki/user_authentication_basic/controller/ViewController.java
+++ user_authentication_basic/src/main/java/io/github/auth/user/mayki/user_authentication_basic/controller/ViewController.java
@@ -1,4 +1,22 @@
 package io.github.auth.user.mayki.user_authentication_basic.controller;
 
+import io.github.auth.user.mayki.user_authentication_basic.security.CustomAuthentication;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class ViewController {
+
+    @GetMapping
+    @ResponseBody
+    public String home(Authentication authentication) {
+        if (authentication instanceof CustomAuthentication customAuthentication) {
+            System.out.println(customAuthentication.getUsuario());
+        }
+        return "Olá " + authentication.getName();
+    }
+
 }
